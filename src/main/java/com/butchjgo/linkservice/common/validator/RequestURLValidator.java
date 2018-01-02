00d6f490fc51fe7d8747b130a221310a51bd0751
root <damducthoai@gmail.com>
2018-01-02 11:58:11 +0000

    implement url request validator

--- src/main/java/com/butchjgo/linkservice/common/validator/RequestURLValidator.java
+++ src/main/java/com/butchjgo/linkservice/common/validator/RequestURLValidator.java
@@ -1,16 +1,30 @@
 package com.butchjgo.linkservice.common.validator;
 
+import com.butchjgo.linkservice.common.domain.RequestURL;
+import com.butchjgo.linkservice.common.pool.Pool;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import javax.annotation.Resource;
+
+@Service(value = "requestURLValidator")
 public class RequestURLValidator implements Validator {
+
+    @Resource(name = "badURLPool")
+    Pool<String> badURLPool;
+
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return RequestURL.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        String url = RequestURL.class.cast(target).getUrl();
+        if (badURLPool.contain(url)) {
+            // TODO refactoring message and code
+            errors.reject("400", "bad request url");
+        }
     }
 }
