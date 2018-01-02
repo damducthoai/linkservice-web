eb45ad1eb70f286c609a6d14051e728f10d51407
root <damducthoai@gmail.com>
2018-01-02 13:46:50 +0000

    add json support for creating post data

--- src/test/java/com/butchjgo/linkservice/LinkserviceApplicationTests.java
+++ src/test/java/com/butchjgo/linkservice/LinkserviceApplicationTests.java
@@ -1,11 +1,13 @@
 package com.butchjgo.linkservice;
 
+import com.butchjgo.linkservice.common.domain.RequestURL;
 import com.butchjgo.linkservice.service.RegisterService;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,28 +30,36 @@ public class LinkserviceApplicationTests {
     public void testSupportedURL() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost post = new HttpPost(serverURL + "linkservice");
-
-        StringEntity entity = new StringEntity("{\"url\":\"test\",\"password\":\"test\"}");
+        JSONObject jsonObject = new JSONObject(new RequestURL("test", "test"));
+        StringEntity entity = new StringEntity(jsonObject.toString());
         post.setEntity(entity);
         post.addHeader("Content-Type", "application/json");
         CloseableHttpResponse response = httpClient.execute(post);
+
+        //evaluate result
         assert response.getStatusLine().getStatusCode() == HttpStatus.BAD_REQUEST.value();
     }
 
     @Test
     public void testFshare() throws IOException {
+        // init resource
         final String fsharePattern = "https://www.fshare.vn/file/[a-zA-Z0-9]+";
         patternRegister.register(fsharePattern);
-
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost post = new HttpPost(serverURL + "linkservice");
+        JSONObject jsonObject = new JSONObject(new RequestURL("https://www.fshare.vn/file/P3YBDNV9AFYCJF6", "no"));
 
-        StringEntity entity = new StringEntity("{\"url\":\"https://www.fshare.vn/file/P3YBDNV9AFYCJF6\",\"password\":\"test\"}");
+        StringEntity entity = new StringEntity(jsonObject.toString());
         post.setEntity(entity);
         post.addHeader("Content-Type", "application/json");
         CloseableHttpResponse response = httpClient.execute(post);
 
-        patternRegister.unregister(fsharePattern);
+        // close resource
+        response.close();
+        httpClient.close();
+        patternRegister.unregister("Xin chao" + fsharePattern);
+
+        //evalue result
         assert response.getStatusLine().getStatusCode() == HttpStatus.CREATED.value();
 
     }
