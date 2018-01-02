fec8ce6da3790a4b189be9e8da1a7186721808b6
root <damducthoai@gmail.com>
2018-01-02 11:58:31 +0000

    register validator for controller

--- src/main/java/com/butchjgo/linkservice/web/LinkService.java
+++ src/main/java/com/butchjgo/linkservice/web/LinkService.java
@@ -4,9 +4,12 @@ import com.butchjgo.linkservice.common.domain.RequestURL;
 import com.butchjgo.linkservice.common.domain.RequestURLResult;
 import com.butchjgo.linkservice.common.exception.BadRequestException;
 import com.butchjgo.linkservice.service.UniqueService;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,6 +22,9 @@ public class LinkService {
     @Resource(name = "uniqueService")
     UniqueService uniqueService;
 
+    @Resource(name = "requestURLValidator")
+    Validator requestURLValidator;
+
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     RequestURLResult doPost(@Valid @RequestBody RequestURL req, BindingResult result) throws BadRequestException {
@@ -28,5 +34,11 @@ public class LinkService {
         String id = uniqueService.get();
         // TODO refactoring message instead of hard code
         return new RequestURLResult(id, "Request success");
+    }
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(requestURLValidator);
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 }
