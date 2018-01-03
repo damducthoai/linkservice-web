515c4262b813d614bfa6300f1beebf695e7e7a1e
root <damducthoai@gmail.com>
2018-01-03 22:02:56 +0000

    do refactoring to lambda exp

--- src/main/java/com/butchjgo/linkservice/service/UniqueServiceImpl.java
+++ src/main/java/com/butchjgo/linkservice/service/UniqueServiceImpl.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
 
 @Service(value = "uniqueService")
 public class UniqueServiceImpl implements UniqueService {
 
     final String baseString = LocalTime.now().toString();
-    AtomicLong atomicLong = new AtomicLong();
+    final AtomicLong atomicLong = new AtomicLong();
+    private Supplier<String> stringSupplier = () -> DigestUtils.md2Hex(String.valueOf(atomicLong.incrementAndGet()).concat(baseString));
 
     @Override
-    synchronized public String get() {
-        String cur = String.valueOf(atomicLong.incrementAndGet()).concat(baseString);
-        return DigestUtils.md2Hex(cur);
+    public String get() {
+        return stringSupplier.get();
     }
 }
