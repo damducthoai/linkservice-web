24ac128abca9b33f5dc8d0b49e7231b9080d26f9
root <damducthoai@gmail.com>
2018-01-09 21:59:16 +0000

    add time for result

--- src/main/java/com/butchjgo/linkservice/service/ResultMessageHandlerService.java
+++ src/main/java/com/butchjgo/linkservice/service/ResultMessageHandlerService.java
@@ -8,12 +8,22 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.Map;
+import java.util.TimeZone;
 
 @Service
 public class ResultMessageHandlerService {
+
+    DateFormat sdfDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+    TimeZone tz = TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
 
     @Autowired
     ObjectMapper objectMapper;
@@ -27,8 +37,16 @@ public class ResultMessageHandlerService {
     public ResultMessageHandlerService() {
     }
 
+    @PostConstruct
+    void custom() {
+        sdfDate.setTimeZone(tz);
+    }
+
     @JmsListener(destination = "result",containerFactory = "resultJmsListenerContainerFactory")
     void onMessage(ResultData rs) throws IOException {
+
+        rs.setTime(new Date().toString());
+
         System.out.println(String.format("received: %s",rs.getResult()));
         String id = rs.getClientid();
 
